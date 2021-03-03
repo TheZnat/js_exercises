e0794559ec847f4801d0c14d3d1cb38f2c186580
Max Kuzinov <60483614+TheZnat@users.noreply.github.com>
2021-03-03 19:35:25 +0300

    Add files via upload

---
+++ 1_homework.js
@@ -0,0 +1,27 @@
+let num = 98;
+fun(num);
+function fun(num){
+    for (let i = 0; i <= num; i++){
+        switch (i){
+            case 5:
+                console.log("Пять");
+                break;
+            case 13:
+                console.log("Тринадцать");
+                break;
+            case 22:
+                console.log("Двадцать два");
+                break;
+            case 35:
+                console.log("тридацать пять");
+                break;
+            case 98:
+                console.log("девяносто восемь");
+                break;
+            default:
+                console.log(i);
+                break;
+        }
+    }
+}
+
