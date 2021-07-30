efeb4f0c30d13af2f1398589043ed571431a965b
Max Kuzinov <60483614+TheZnat@users.noreply.github.com>
2021-07-30 03:57:07 +0300

    Add files via upload

--- three_best_jumps.js
+++ three_best_jumps.js
@@ -11,7 +11,7 @@ let bubbleSort = (arr) => {
             if(arr[i] > arr[i + 1]){
                 swap = arr[i];
                 arr[i] = arr[i + 1];
-                arr[i + 1] = swap
+                arr[i + 1] = swap;
             }
         }
     }
